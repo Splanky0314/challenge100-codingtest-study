8fb46a754a9bad70bfbf89aaef182efc156fb424
splanky0314 <splanky0314@gmail.com>
2026-02-05 22:39:52 +0900

    [BOJ] 노트북 세 대를 가지고 왔다 / 브론즈 5 / 1분

---
+++ splanky0314/CONTEST/25_SUAPC/33515_노트북_세_대를_가지고_왔다.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	// 2/5 22:36
+
+	int t1, t2;
+	cin >> t1 >> t2;
+
+	cout << min(t1, t2);
+	
+}
